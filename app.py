c711a03db65f7fc00341cc33c8cc3f338b567d3d
Patrick Smyth <patrick.smyth@chainguard.dev>
2024-11-19 10:12:59 -0500

    inky to linky

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     octopuses = '🐙' * 10
-    return f'<h1>Inky Is Best</h1><p>{octopuses}</p>'
+    return f'<h1>Linky Is Best</h1><p>{octopuses}</p>'
 
 
 if __name__ == "__main__":
